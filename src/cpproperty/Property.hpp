2af594a0eef65b04bf80772acdb293545d3264d6
g <gtmalmgren@gmail.com>
2018-08-09 08:04:38 -0700

    Added operators

--- src/cpproperty/Property.hpp
+++ src/cpproperty/Property.hpp
@@ -4,6 +4,27 @@
 
 namespace cpproperty {
 
+    namespace detail {
+        // SFINAE test
+        template<typename T>
+        class has_operator {
+#define HAS_BINARY_OP(name, op)                                                                    \
+private:                                                                                           \
+    template<typename C>                                                                           \
+    static auto name##_(C* c1, C* c2)->decltype((*c1)op(*c2));                                     \
+                                                                                                   \
+    template<typename C>                                                                           \
+    static void name##_(...);                                                                      \
+                                                                                                   \
+public:                                                                                            \
+    const static int name = std::is_same<decltype(name##_<T>((T*)0, (T*)0)), T>::value;
+            HAS_BINARY_OP(add, +);
+            HAS_BINARY_OP(sub, -);
+            HAS_BINARY_OP(mul, *);
+            HAS_BINARY_OP(div, /);
+        };
+    };
+
     enum Access { PUBLIC, PROTECTED, PRIVATE };
 
     template<typename Parent, typename T, Access getter = PUBLIC, Access setter = PUBLIC>
@@ -41,6 +62,20 @@ namespace cpproperty {
             return val;
         };
 
+#define OP_USE(ACCESS, name, op)                                                                   \
+    template<typename T2 = T>                                                                      \
+    typename std::enable_if<(getter == ACCESS) && (detail::has_operator<T>::name), T2>::type       \
+    operator op(const T2& val) {                                                                   \
+        return this->get(this->value) op val;                                                      \
+    };                                                                                             \
+                                                                                                   \
+    template<typename T2 = T>                                                                      \
+    const typename std::enable_if<(setter == ACCESS) && (detail::has_operator<T>::name),           \
+                                  T2>::type&                                                       \
+    operator op##=(const T2& val) {                                                                \
+        return this->value = this->set(this->value op val);                                        \
+    };
+
 #define GETTER_SETTTER(access, ACCESS)                                                             \
     access:                                                                                        \
     template<typename T2 = T>                                                                      \
@@ -51,7 +86,11 @@ namespace cpproperty {
     template<typename T2 = T>                                                                      \
     operator const typename std::enable_if<setter == ACCESS, T2>::type&() {                        \
         return this->get(value);                                                                   \
-    };
+    };                                                                                             \
+    OP_USE(ACCESS, add, +);                                                                        \
+    OP_USE(ACCESS, sub, -);                                                                        \
+    OP_USE(ACCESS, mul, *);                                                                        \
+    OP_USE(ACCESS, div, /);
 
         GETTER_SETTTER(public, PUBLIC);
         GETTER_SETTTER(protected, PROTECTED);
--- tests/Example.cpp
+++ tests/Example.cpp
@@ -11,8 +11,15 @@ class Person {
 public:
     Property<Person, int, cpproperty::PUBLIC, cpproperty::PUBLIC> age;
 
+    Property<Person, std::string, cpproperty::PUBLIC, cpproperty::PROTECTED> name;
+
     Person()
-      : age(this){};
+      : age(this)
+      , name(this){};
+
+    void setName(std::string name) {
+        this->name.value = name;
+    };
 };
 
 TEST_CASE("example", "[example]") {
@@ -25,7 +32,30 @@ TEST_CASE("example", "[example]") {
         p.age = 1;
         REQUIRE(1 == p.age);
 
-        p.age = 1 + p.age;
+        p.age = p.age + 1;
         REQUIRE(2 == p.age);
+
+        // compiler error
+        // p.age = 1 + p.age;
+        // REQUIRE(2 == p.age);
+
+        /* p.age += 1; */
+        /* REQUIRE(3 == p.age); */
+
+        /* p.age *= 2; */
+        /* REQUIRE(6 == p.age); */
+
+        /* p.age -= 2; */
+        /* REQUIRE(4 == p.age); */
+
+        /* p.age /= 2; */
+        /* REQUIRE(2 == p.age); */
+    }
+    SECTION("strings") {
+        p.setName("hello");
+        REQUIRE((p.name == "hello"));
+
+        // compiler error
+        // p.name = "goodbye";
     }
 }
